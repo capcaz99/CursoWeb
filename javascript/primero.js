8dc2b30db228cb47dd5ecb532a6a26a52b8e1d12
CarlosAburto <carlos.ab.pe@gmail.com>
2017-06-06 21:49:16 -0500

    Mover 2

---
+++ javascript/primero.js
@@ -0,0 +1,7 @@
+var userName = prompt("Cual es tu nombre?");
+var userLastName = prompt("Cual es tu apellido?");
+var age = prompt("Cual es tu edad?");
+console.log("Tu nombre completo es "+userName+" "+userLastName);
+console.log("Tu edad es "+age);
+
+
